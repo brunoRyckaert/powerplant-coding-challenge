06c50357e60f634818dcc871b93aca01eb540c6c
Bruno <info@brunit.be>
2024-05-31 13:34:12 +0200

    ProductionPlanItem

--- PowerPlant.Api/Controllers/PowerPlantAPiController.cs
+++ PowerPlant.Api/Controllers/PowerPlantAPiController.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using PowerPlant.Api.Data;
 using PowerPlant.Domain;
+using PowerPlant.Domain.Models;
 
 namespace PowerPlant.Api.Controllers
 {
@@ -22,10 +23,11 @@ namespace PowerPlant.Api.Controllers
         }
 
         [HttpPost("productionplan")]
-        public string PostProductionPlan(PayloadDTO payloadDto)
+        public IEnumerable<ProductionPlanItem> PostProductionPlan(PayloadDTO payloadDto)
         {
             var payload = payloadDto.ToPayload();
-            return "Hello production plan";
+            var productionPlan = new List<ProductionPlanItem> { new ProductionPlanItem { Name = "windpark1", Power = 90.0M }, new ProductionPlanItem { Name = "gasfiredbig1", Power = 460.0M } };
+            return productionPlan;
         }
     }
 }
---
+++ PowerPlant.Domain/Models/ProductionPlanItem.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.Json.Serialization;
+
+namespace PowerPlant.Domain.Models
+{
+    public class ProductionPlanItem
+    {
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+        [JsonPropertyName("p")]
+        public decimal Power { get; set; }
+    }
+}
